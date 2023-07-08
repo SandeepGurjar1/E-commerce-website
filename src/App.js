039c2aa5a9ac6f47b13db1db0da0ef8f9b2e05e6
Sandeep Gurjar <101051507+SandeepGurjar1@users.noreply.github.com>
2023-07-08 23:11:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import Home from "./components/Home";
 import Navbar from "./components/Navbar";
-import {Route, Routes } from "react-router-dom";
+import {Route, Routes, Switch } from "react-router-dom";
 import Products from "./components/Products";
 import Product from "./components/Product";
 import Cart from "./components/Cart";
@@ -13,17 +13,16 @@ import Contact from "./components/Contact";
 function App() {
   return (
     <>
-      <Home />
-      <Routes>
-      <Route exact path="/register" element={<Register/>} />
+      <Switch>
+        <Route path="/" element={<Home/>}/>
+        <Route exact path="/register" element={<Register/>} />
         <Route exact path="/login" element={<Login/>} />
-        <Route exact path="/" element={<Home/>}/>
         <Route exact path="/products" element={<Products/>} />
         <Route exact path="/cart" element={<Cart/>} />
         <Route exact path="/products/:id" element={<Product/>} />
         <Route exact path="/about" element={<About/>} />
         <Route exact path="/contact" element={<Contact/>} />
-      </Routes>
+      </Switch>
     </>
   );
 }
